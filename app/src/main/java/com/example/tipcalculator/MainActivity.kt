b4f947bb0be2ed8171f24e2ba0dd097175f1d02b
mohamed oussama <mohamedoussama.bougueddach@gmail.com>
2021-10-17 20:01:46 +0100

    Add calculateTip() to calculate tip when the button is clicked

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -3,15 +3,37 @@ package com.example.tipcalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.calculateButtom.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val costOfService = binding.costOfService.text.toString().toDoubleOrNull()
+        if (costOfService == null) {
+            binding.amountToTip.text = ""
+            return
+        }
+        val selectedService = binding.options.checkedRadioButtonId
+        val tipPrecentage = when (selectedService) {
+            R.id.amazing_option -> 0.2
+            R.id.good_option -> 0.18
+            else -> 0.15
+        }
+        var tip = costOfService * tipPrecentage
+        if (binding.roundTip.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.amountToTip.text = getString(R.string.tip_amount, formattedTip)
     }
 
 
